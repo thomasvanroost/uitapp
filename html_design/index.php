bca4eb9d30d39cd23feab8f3d1bbf3b4c261265e
Thomas Van Roost <thomasvanroost@gmail.com>
2013-05-17 22:26:06 +0200

    toevoegen design

---
+++ html_design/index.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/gumby.css" />
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+<body>
+	<nav class="navbar" id="#nav1">
+	
+		<!--
+		dit toon het menu wanneer je op kleinere schermgrootste kijkt.
+		<a class="toggle" gumby-trigger="#nav1 > .row > ul" href="#"><i class="icon-menu" ></i></a> -->
+		
+		<div class="row">
+		<div>
+			<h1 class="four columns logo"> 
+			<a  href="#"><img src="assets/images/uitLogo80x48.png" alt="UitApp"> </a>
+			</h1>
+			
+		<ul class="">		
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Zoeken</a> </li>
+			<li class=""><a href="#">Ontdekken</a></li> 
+			<li> 
+			<div class="append field push_two">
+					<input class="xwide text input" type="text" placeholder="Activiteit, plaats, dag,..." />
+					<div class="medium info btn"><a href="#">Zoek</a></div>
+			</div>
+			</li> 
+		</ul>
+		
+		</div>
+		</div>
+	</nav>
+
+	 <div class="row">
+	 	<div class="ten columns">
+	 	<h1>Vandaag in Mechelen </h1>
+	 	</div>
+	 	
+	 	<div class="ten columns">
+	 	<p>Wat wil je doen? </p>
+	 	</div>
+	 	
+	 
+	 	
+	 	<div class="seven columns">
+	 	<div class="small default btn"><a href="#">Toneel </a> </div>
+	 	<div class="small default btn"><a href="#">Tentoonstelling</a></div>
+	 	<div class="small default btn dans"><a href="#">Dans </a></div>
+	 	<div class="small default btn fuif"><a href="#">Fuif </a></div>
+	 	<div class="small default btn"><a href="#">Workshop </a></div>
+	 	<div class="small success btn"><a href="#">Film </a></div>
+	 	</div>
+	 	
+	 	
+    
+ 
+    <div class="row">
+	    
+	    <section class="ten columns">
+	   <a href="#">	
+	    <article class="valign row">
+			    
+	      <div>
+	        <img alt="" src="http://media.uitdatabank.be/20120709/4eb59a9a-5872-4ee4-bd6a-dd31b921392d.jpg" width="145" />
+	      </div>
+	      <div>
+	        <h4>Hier is wat ik denk  </h4>
+	        <p>Stadsschouwburg Mechelen </p>
+	      </div>
+	      </article>
+	      </a>
+	      
+	      
+	    <article class="valign row">
+	      <div>
+	        <img alt="" src="http://media.uitdatabank.be/20120709/4eb59a9a-5872-4ee4-bd6a-dd31b921392d.jpg" width="145" />
+	      </div>
+	      <div>
+	        <h4 >Hier is wat ik denk</h4>
+	        <p>Stadsschouwburg Mechelen </p>
+	      </div>
+	      </article>
+	      
+	      
+	      <article class="valign row">
+	      <div>
+	        <img alt="" src="http://media.uitdatabank.be/20120709/4eb59a9a-5872-4ee4-bd6a-dd31b921392d.jpg" width="145" />
+	      </div>
+	      <div>
+	        <h4>Hier is wat ik denk</h4>
+	        <p>Stadsschouwburg Antwerp</p>
+	      </div>
+	      </article>
+	    </section>
+	    
+    </div>
+	 </div>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	<!-- AUTO reload script livereload -->
+		<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+	
+</body>
+</html>
